5d31a9989fee6e26ca69f87663d3daeda363f3bb
FherEnrique <enquique500@gmail.com>
2021-06-17 22:34:41 -0600

    primer avance probado :white_check_mark:

--- app/Http/Controllers/ActionController.php
+++ app/Http/Controllers/ActionController.php
@@ -2,50 +2,72 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Action;
 use Illuminate\Http\Request;
+use App\Http\Controllers\DeathController;
+use Illuminate\Support\Facades\DB;
 
 class ActionController extends Controller
 {
     public function history()
     {
-        dd(session('listLive'));
-        dd(session('listDeath'));
-        dd(session('day'));
-        /*
-        @if (empty(session('listaVivo')))
-        {{ redirect()->to('')->send() }}
-    @endif
-    @section('content')
-        <div style="font-size: 0px"><!-- SISTEMA DE MUERTES y de dias sup -->
+        //zona de validaciones
+        if (empty(session('listLive'))) {
+            redirect()->to('/play/')->send();
+        }
+        $listLive = json_decode(session('listLive'));
+        if (count($listLive) <= 1) {
+            redirect()->to('ganador/')->send();
+        }
+        $listDeath = json_decode(session('listDeath'));
+        $day = json_decode(session('day'));
+        #Zona de los muertos
+        //Zona de variables
+        $helper = array(); //Ver quienes siguen vivos
+        $permanentHelper = array(); // PARA actualizar la variable permanente
+        $deathNow = array(); //PARA lista de muertos
+        $destailsDeath = app(DeathController::class)->listAllDeath();
 
-            {{ $a = session('diaActual') }}
-            {{ $string = session('listaVivo') }}
-            {{ $sting2 = session('listaMuerto') }}
-            <?php
-                $array = json_decode($string);
-                $array2 = json_decode($sting2);
-                if (count($array) <= 1) {?>
-                {{  redirect()->to('ganador/')->send() }}
-                <?php }
-                use Illuminate\Support\Facades\DB;
-                $aux = array(); //Ver quienes siguen vivos
-                $ahora = array(); // PARA actualizar listaMuerto
-                $anuncio = array(); //PARA lista de muertos
-                $odio = DB::select("SELECT descripcion FROM muertes");
+        foreach ($listLive as $destailsLive) {
+            $destailsLive->dias = $day;
+            if ($destailsLive->health < rand(1, 100)) { //Abandonad cualquier esperanza
+                array_push($deathNow, "Suena un cañon a lo lejos " . $destailsLive->name . " murio: " . $destailsDeath[rand(0, count($destailsDeath) - 1)]->description);
+                array_push($permanentHelper, $destailsLive);
+            } else {
+                array_push($helper, $destailsLive);
+            }
+        }
+        array_push($listDeath, $permanentHelper); //UNIR los muertos de antes con los de ahora
+        #zona de las acciones de ahora
+        $actionNow = array(); //Ver las aciones de ahora
+        $final = array(); //Para enviar a la sesion con los datos de salud actualizados
+        $destailsActions = DB::select("SELECT a.id, a.number_character,a.health,a.description FROM actions a LEFT JOIN helpers x ON a.id = x.id_son WHERE x.id_son IS NULL");
 
-                foreach ($array as $key) {
-                    $key->dias = $a;
-                    if ($key->salud < rand(1,100)) { //Abandonad cualquier esperanza
-                        array_push($anuncio, "Suena un cañon a lo lejos ".$key->nombre." tuvo una ".$odio[rand(0, count($odio)-1)]->descripcion);
-                        array_push($ahora, $key);
-                    }else{
-                        array_push($aux, $key);
-                    }
-                }
-                array_push($array2, $ahora); //UNIR los muertos de antes con los de ahora
-            ?>
-
-            <?php
+        foreach ($helper as $key) {
+            $index = rand(0, count($destailsActions) - 1);
+            array_push($actionNow, $key->name . " realiza la " . $destailsActions[$index]->description);
+            $key->health = $key->health + $destailsActions[$index]->health;
+            $key->health = ($key->health < 0) ? 0 : $key->health;
+            $key->health = ($key->health > 100) ? 100 : $key->health;
+            array_push($final, $key);
+        }
+        //Actualizaciones en los session
+        $day++;
+        session(['day' => $day, 'listDeath' => json_encode($listDeath), 'listLive' => json_encode($final)]);
+        return view('history', compact('day', 'actionNow', 'deathNow', 'final'));
+    }
+    public function win()
+    {
+        if (session('listDeath') != "") {
+            $winner = json_decode(session('listLive'));
+            return view('winners', compact('winner'));
+        } else {
+            redirect()->to('/history/')->send();
+        }
+    }
+}
+/* 
+<?php
             $accion = array(); //Ver las aciones de ahora
             $final = array(); //Para enviar a la sesion con los datos de salud actualizados
             foreach ($aux as $key) {
@@ -53,6 +75,11 @@ class ActionController extends Controller
                     $amor = DB::select("SELECT a.id, a.cant_personajes, a.salud, a.descripcion FROM auxiliares x INNER JOIN acciones a ON x.id_hijo = a.id WHERE x.id_padre = ".$key->arbol);
                 }else{
                     $amor = DB::select("SELECT a.id, cant_personajes,salud,descripcion FROM acciones a LEFT JOIN auxiliares x ON a.id = x.id_hijo WHERE x.id_hijo IS NULL");
+                    DB::table('actions')
+                        ->select('actions.id', 'actions.number_character', 'actions.health', 'actions.description')
+                        ->join('helpers', 'actions.id', '=', 'helpers.id_son')
+                        ->whereNull('helpers.id_son')
+                        ->get();
                 }
                 $indice = rand(0, count($amor)-1);
                 array_push($accion, $key->nombre." realiza la ".$amor[$indice]->descripcion);
@@ -69,7 +96,4 @@ class ActionController extends Controller
                 array_push($final, $key);
             }
             ?>
-        */
-        return view('history');
-    }
-}
+*/
--- resources/views/winners.blade.php
+++ resources/views/winners.blade.php
@@ -1,16 +1,13 @@
 @extends('layout')
     @section('content')
-        <div style="font-size: 0px;">
-            {{ $string = session('listaVivo') }}
-            <?php 
-                $array = json_decode($string);
-            ?>
-        </div>
-        @if (count($array) == 0)
-            <h1>TODOS han muerto <br>
-            press F to pay respect
-            </h1>
+        @if (count($winner) == 1)
+            <h1>EL GANADOR ES: {{ $winner[0]->name }}</h1>
         @else
-            <h1>EL GANADOR ES: {{ $array[0]->nombre }}</h1>
+            <h1>TODOS han muerto <br>
+                press F to pay respect
+            </h1>
         @endif
+        <a href="/destroyTheGame/">
+            <button>Jugar de nuevo</button>
+        </a>
     @endsection
