2624fbe796240a88adb473750bbf93707454b487
Mario <mariobeltran1999@gmail.com>
2021-06-26 13:55:01 -0600

    refactor design :lipstick:

--- resources/views/layouts/homeLayout.blade.php
+++ resources/views/layouts/homeLayout.blade.php
@@ -1,26 +1,5 @@
 @extends('..layouts.layout')
 @section('content')
-{{-- <div class="text-center">
-    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-        <header class="masthead mb-auto">
-            <div class="inner">
-                <h3 class="masthead-brand">ATheneus</h3>
-                <nav class="nav nav-masthead justify-content-center">
-                    <a class="nav-link" href="/play/">Jugar</a>
-                    <a class="nav-link" href="/about/">Acerca de</a>
-                </nav>
-            </div>
-        </header>
-        <main role="main">
-            @yield('container')
-        </main>
-        <footer class="mastfoot mt-auto">
-            <div class="inner">
-                <p>Made with coffe and hard drugs. &#x1F49D;</p>
-            </div>
-        </footer>
-    </div>
-</div> --}}
 <div id="particles-js" class="relative h-96 h-screen">
     <div class="w-full mx-auto text-center absolute z-50">
         <div class="cover-container flex p-3 mx-auto flex flex-col">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,4 +8,5 @@
                 </div>
             </div>
         </div>
+        <script src="{{ asset('js/app.js') }}"></script>
 @endsection
