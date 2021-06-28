22cdcc3c33ba21bea4373065fd51e5d96e9ab423
Mario <mariobeltran1999@gmail.com>
2021-06-27 19:59:14 -0600

    fix design :bug: :lipstick:

--- resources/views/winners.blade.php
+++ resources/views/winners.blade.php
@@ -1,22 +1,22 @@
 @extends('..layouts.homeLayout')
-    @section('content')
-        <div class="container w-4/5 mx-auto md:w-1/2">
-            <div class="p-7 bg-gray-600 shadow-2xl bg-opacity-20 text-white border border-white border-2 border-b-4 border-opacity-50 rounded-3xl">
-                @if (false)
-                    <p class="text-center text-8xl mx-auto p-20">EL GANADOR ES </p>
-                    <p class="text-center text-9xl mx-auto mt-8">{{ $winner[0]->name }}</p>
+    @section('container')
+            <div class="p-7 w-full bg-gray-600 shadow-2xl bg-opacity-20 mb-8 text-white border border-white border-2 border-b-4 border-opacity-50 rounded-3xl">
+                @if (count($winner) == 1)
+                    <p class="text-center text-6xl mx-auto p-14">EL GANADOR ES </p>
+                    <p class="text-center text-7xl mx-auto mt-8">{{ $winner[0]->name }}</p>
                 @else
-                    <p class="text-center text-8xl mx-auto px-20 py-10">TODOS han muerto</p>
-                    <img src="/img/F.png" alt="F keycap" class="img-f mx-auto">
-                    <p class="text-center text-7xl mx-auto px-20 py-10">Press <span class="text-yellow-400">F</span> to pay respect</p>
+                    <p class="text-center text-6xl mx-auto p-14">Todos han muerto...</p>
+                    <img src="/img/F.png" alt="F keycap" class="img-f mx-auto" width="100" height="100">
+                    <p class="text-center text-6xl mx-auto p-14">Press <span class="text-yellow-400">F</span> to pay respect</p>
                 @endif
-                <div class="flex-center flex">
+                <div class="flex-center flex mt-8 mb-8">
                     <div>
                         <a href="/destroyTheGame/">
-                            <button class="btn btn-dark uppercase text-center border-2 border-white rounded-3xl text-2xl mt-36 p-4">Jugar de nuevo</button>
+                            <button class="bg-transparent hover:bg-green-800 text-blue-dark p-2 font-semibold hover:text-white px-4 border border-blue hover:border-transparent rounded-3xl text-2xl">
+                                <i class="fas fa-redo-alt mr-4 text-center"></i>  JUGAR DE NUEVO
+                            </button>
                         </a>
                     </div>
                 </div>
             </div>
-        </div>
     @endsection
